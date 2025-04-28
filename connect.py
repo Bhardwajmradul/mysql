4a6f64d8819199cd82301499e71c89149003f318
Bhardwajmradul <mradul.bhardwaj2003@gmail.com>
2025-04-28 15:03:51 +0530

    final commit

--- connect.py
+++ connect.py
@@ -1,26 +1,31 @@
 import mysql.connector
 mydb = mysql.connector.connect(
-  host = "gateway01.ap-southeast-1.prod.aws.tidbcloud.com",
-  port = 4000,
-  user = "2n8ca3An3YaETcW.root",
-  password = "cvS715s6tLemjHl3",
+  host = "sql12.freesqldatabase.com",
+  port = 3306,
+  user = "sql12775336",
+  password = "HprDPIFI9z",
+  database="sql12775336"
   # ssl_ca = "/etc/ssl/certs/ca-certificates.crt",
   # ssl_verify_cert = True,
   # ssl_verify_identity = True
 )
-# mydb.cursor().execute("create database if not exists school")
-mydb.cursor().execute("use school")
-db = mydb.cursor()
-# db.execute("Truncate table records")
+db=mydb.cursor()
+# # db.execute("create table records(record_no int not null,record_name char not null,record_date date not null,Total_Entries int not null,Emp_Id int not null)")
 # mydb.commit()
-query="insert into records (record_no,record_name,record_date,no_of_entries,Emp_Id) values (%s,%s,%s,%s,%s)"
+# # db.execute("create table test(number int)")
 import random
 import time
 from datetime import datetime
 
-db.execute('insert into records (record_no,record_name,record_date,no_of_entries,Emp_Id) values (1,"hello","2025-01-01",111,101)')
-mydb.commit()
-# for i in range(1,2):
-#   # data = (i,str(time.time()),str(datetime.now().date()),random.randint(1,999999),101)
+query='insert into records (record_no,record_name,record_date,Total_Entries,Emp_Id) values (%s,%s,%s,%s,%s)'
+
+# for i in range(1,10200):
+#   data = (i,str(time.time()),str(datetime.now().date()),random.randint(1,999999),101)
 #   db.execute(query,data)
 # mydb.commit()
+query='select * from records limit %s offset %s'
+db.execute(query,(10,20))
+l=db.fetchall()
+print(len(l))
+for record in l:
+  print(record)
